7051f1bd7a0f393eb054bf2e6efa6e2b6f772e07
Sid Hackney <snackney@gmail.com>
2017-10-20 18:43:59 -0400

    First commit

---
+++ site.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('.day_header').click((e) => {
+    $('.sched-grids').addClass('hidden');
+    $('.day_header').removeClass('active');
+    $(e.target).addClass('active');
+    const tableID = `#${e.target.id}_table`;
+    $(tableID).removeClass('hidden');
+  });
+});
